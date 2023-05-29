33fe45dbf65387c7142bc52f6ada16c2dad285c6
Rio Saputro <rio.ae23@gmail.com>
2023-05-29 19:28:56 +0700

    add

---
+++ book_categories.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Librarow</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/user_page.css">
+    <link rel="stylesheet" href="css/book_categories.css">
+</head>
+
+<body style="background-color: #D0D0D0;">
+
+    <!--Navbar-->
+    <nav class="navbar sticky-top navbar-expand-sm navbar-dark" style="color: #D0EFFF;">
+        <div class="container-fluid ">
+            <a class="navbar-brand " href="#">LIBRAROW</a>
+            <div class="nav navbar-custom" id="navbarSupportedContent" style="color: #D0EFFF !important;">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="user_page.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Profile</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid catbox">
+      <div class="cattitle">CATEGORIES</div>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+       <br>
+    </div>
+
+
+</body>
+
+</html>
