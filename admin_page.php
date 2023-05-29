4cc3084c2597ca0194015ebbab6bd4ef9b6a504d
Rio Saputro <rio.ae23@gmail.com>
2023-05-29 22:29:43 +0700

    revisiAdminPage

--- admin_page.php
+++ admin_page.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Librarow</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/user_page.css">
+    <link rel="stylesheet" href="css/admin_page.css">
 </head>
 
 <body style="background-color: #D0D0D0;">
