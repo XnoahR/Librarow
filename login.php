efa35ab7bf4187f1bc3b8c569ccff13a6bb4f5d1
Rio Saputro <rio.ae23@gmail.com>
2023-05-29 20:46:26 +0700

    LoginPage

--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,12 +8,28 @@
     <title>Librarow Login</title>
     <link rel="stylesheet" href="css/login.css">
 </head>
+
 <body>
     <div class="logincontainer">
         <div class="circle">
             <div class="logo">xxx </div>
         </div>
         <div class="title">LOGIN</div>
+        <!-- sementara -->
+        <form action="" method="post">
+            <div class="textlog">Username <br>
+        <input type="text" name="username" id="username">
+            <div class="textlog">Password <br>
+        <input type="password" name="password" id="password">
+        <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+        <button type="submit">Login</button>
+        </form>
+        </div>
+        <a href="">Sign up</a>
+        <a href="" style="margin-left:125px;">Forgot password?</a>
     </div>
 </body>
+
 </html>
