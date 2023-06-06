d5c7aed79929a8e8049b48d07091affe09d70222
Rio Saputro <rio.ae23@gmail.com>
2023-06-07 03:37:07 +0700

    Create book_list.php

---
+++ book_list.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Librarow</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/user_page.css">
+    <link rel="stylesheet" href="css/book_categories.css">
+</head>
+
+<body style="background-color: #D0D0D0;">
+
+    <!--Navbar-->
+    <nav class="navbar sticky-top navbar-expand-sm navbar-dark" style="color: #D0EFFF;">
+        <div class="container-fluid ">
+            <a class="navbar-brand " href="#">LIBRAROW</a>
+            <div class="nav navbar-custom" id="navbarSupportedContent" style="color: #D0EFFF !important;">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="user_page.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="book_categories.php">Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="user_profile.php">Profile</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="cattitle">book categories</div>
+
+    <div class="blshelf">
+        <div class="blimgcon">
+
+            <img src="img/stevejob.png" alt="">
+        </div>
+        <div class="bltextcon">
+            <div class="bltitle">TITLE</div>
+            <div class="bltextbox">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+            </div>
+            <div class="blauthor">RAY</div>
+            <div class="blbuttoncon"><button class="blbutton"><a href="borrow_book.php">BORROW</a></button></div>
+        </div>
+    </div>
+    <div class="blshelf">
+        <div class="blimgcon">
+
+            <img src="img/stevejob.png" alt="">
+        </div>
+        <div class="bltextcon">
+            <div class="bltitle">TITLE</div>
+            <div class="bltextbox">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+            </div>
+            <div class="blauthor">RAY</div>
+            <div class="blbuttoncon"><button class="blbutton"><a href="borrow_book.php">BORROW</a></button></div>
+        </div>
+    </div>
+    
+
+
+    </div>
+    <a href="notifikasi_user.php">
+        <img src="img/bell.png" class="notification-icon" alt="Notification Bell">
+    </a>
+
+
+</body>
+
+</html>
+
+
