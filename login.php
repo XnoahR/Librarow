67090f0f1dc62f6c5d3934b1aa3eaa937429c04d
Tulus Toto Raharjo <totoraharjotulus@gmail.com>
2023-06-16 23:46:50 +0700

    Update login.php

--- login.php
+++ login.php
@@ -31,15 +31,14 @@ if(isset($_POST["login"])){
         setcookie('username',$username,time()+3600);
         setcookie('password',$password,time()+3600);
         header("Location:admin_page.php");
-            exit;
+        exit;
     }
     else{
-        echo "username atau kata sandi salah";
+        $errorMessage = "Username atau kata sandi salah";
     }
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -49,31 +48,93 @@ if(isset($_POST["login"])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Librarow Login</title>
     <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <style>
+        .logo {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .animate__animated {
+            animation-duration: 1s;
+        }
+
+        .animate__rotateIn {
+            animation-name: rotateIn;
+            animation-duration: 2s;
+            animation-iteration-count: infinite;
+        }
+
+        @keyframes rotateIn {
+            0% {
+                transform: rotate(0deg);
+            }
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+
+        .logincontainer {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 85vh;
+        }
+
+        .title {
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+
+        .form-control {
+            margin-bottom: 15px;
+        }
+
+        .btn-primary {
+            width: 100%;
+        }
+
+        .forgot-password {
+            margin-left: 0px;
+        }
+    </style>
 </head>
 
 <body>
     <div class="logincontainer">
         <div class="circle">
             <div class="logo">
-                <img src="img/logo.png" width="30" alt="logo">
+                <img src="img/logo.png" width="30" alt="logo" class="animate__animated animate__rotateIn">
             </div>
         </div>
         <div class="title">LOGIN</div>
-        <!-- sementara -->
+        <?php if(isset($errorMessage)): ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $errorMessage; ?>
+                </div>
+        <?php endif; ?>
         <form action="" method="post">
-            <div class="textlog">Username <br>
-        <input type="text" name="username" id="username">
-            <div class="textlog">Password <br>
-        <input type="password" name="password" id="password">
-        <label>
-        <input type="checkbox" checked="checked" name="remember"> Remember me
-      </label>
-        <button type="submit" name="login">Login</button>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="form-control" required>
+            </div>
+            <div class="form-group form-check">
+                <input type="checkbox" checked="checked" name="remember" id="remember" class="form-check-input">
+                <label for="remember" class="form-check-label">Remember me</label>
+            </div>
+            <button type="submit" name="login" class="btn btn-primary">Login</button>
         </form>
-        </div>
         <a href="register.php">Sign up</a>
-        <a href="" style="margin-left:125px;">Forgot password?</a>
+        <a href="#" class="forgot-password">Forgot password?</a>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
