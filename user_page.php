ca11fc53c2c0b17d5f77a1617ba64b4f744bdb5c
Tulus Toto Raharjo <totoraharjotulus@gmail.com>
2023-06-18 01:04:06 +0700

    update user page css & php

--- user_page.php
+++ user_page.php
@@ -74,6 +74,13 @@ if (!isset($_SESSION['modalShown'])) {
             </div>
         </div>
     </nav>
+    <div id="running-text-carousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <p class="running-text"><strong>Welcome To Our Website!</strong></p>
+            </div>
+        </div>
+    </div>
 
     <div class="bookshelf">
         <div class="bstitle">NEW RELEASE</div>
@@ -163,6 +170,39 @@ if (!isset($_SESSION['modalShown'])) {
     <a href="notifikasi_user.php">
         <img src="img/bell.png" class="notification-icon" alt="Notification Bell">
     </a>
+
+    <footer class="footer bg-dark text-light" style="margin-top: 20px;">
+        <div class="container">
+            <div class="row">
+            <div class="col-lg-4" style="margin-top: 20px;">
+                <h5>About Us</h5>
+                <p>Our library is dedicated to providing a seamless book borrowing experience for our users. With our library management system, you can easily explore our extensive collection of books, place holds, and check out your favorite titles. Our user-friendly interface and efficient processes ensure a hassle-free borrowing process, allowing you to enjoy the pleasure of reading.</p>
+            </div>
+            <div class="col-lg-4" style="margin-top: 20px;">
+                <h5>Contact Us</h5>
+                <ul class="list-unstyled">
+                <li>Email: librarowweb.gmail.com</li>
+                <li>Phone: +6289655847696</li>
+                <li>Address: Kp.Gendingan No.44 Surakarta</li>
+                </ul>
+            </div>
+            <div class="col-lg-4" style="margin-top: 20px;">
+                <h5>Follow Us</h5>
+                <ul class="list-inline">
+                <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                <li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li class="list-inline-item"><a href="#"><i class="fab fa-instagram"></i></a></li>
+                </ul>
+            </div>
+            </div>
+            <div class="row">
+            <div class="col-lg-12 text-center">
+                <p>&copy; 2023 Librarow. All Rights Reserved.</p>
+            </div>
+            </div>
+        </div>
+    </footer>
+
 
 <!-- Script Bootstrap -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
