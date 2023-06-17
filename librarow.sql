7b033c0dea89a204bd010cbb2b96ae82ea5b3b57
Rio Saputro <rio.ae23@gmail.com>
2023-06-17 20:53:04 +0700

    update database

--- librarow.sql
+++ librarow.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 06, 2023 at 10:58 PM
+-- Generation Time: Jun 17, 2023 at 03:52 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -32,7 +32,7 @@ CREATE TABLE `buku` (
   `nama` varchar(50) NOT NULL,
   `pengarang` varchar(50) NOT NULL,
   `available` int(11) NOT NULL,
-  `sampul` blob DEFAULT NULL,
+  `sampul` varchar(100) DEFAULT NULL,
   `kategori` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -41,14 +41,15 @@ CREATE TABLE `buku` (
 --
 
 INSERT INTO `buku` (`id`, `nama`, `pengarang`, `available`, `sampul`, `kategori`) VALUES
-(6, 'December', 'Neck', 5, NULL, 'Biograph'),
-(7, 'Wake Me Up When September Ends', 'Green Day', 10, NULL, 'Biograph'),
-(8, 'Bored to Death', 'blink', 7, NULL, 'Horror'),
-(9, 'Together for the Kids', 'blink', 5, NULL, 'Drama'),
-(10, 'Yellow', 'Gawr Gura', 4, NULL, 'Sociology'),
-(11, 'Lost Kitten', 'Ryou', 1, NULL, 'Romance'),
-(15, 'Soeharto', 'Aidit', 6, '', 'Biograph'),
-(16, 'Naruto', 'Masashi Kishimoto', 2, '', 'Comic');
+(6, 'December', 'Neck Deep', 5, '648908c086017.jpg', 'Biograph'),
+(7, 'Wake Me Up When September Ends', 'Green Day', 10, '6489086b46d89.png', 'Biograph'),
+(8, 'Bored to Death', 'blink', 7, '6489083003283.jpg', 'Horror'),
+(9, 'Together for the Kids', 'blink', 5, '6489083629f7b.jpg', 'Drama'),
+(10, 'Yellow', 'Gawr Gura', 4, '64890530e550a.jpg', 'Sociology'),
+(11, 'Lost Kitten', 'Ryou', 1, '6489091b38e0b.png', 'Romance'),
+(18, 'Naruto V72', 'Masashi Kishimoto', 4, 'naruto.jpg', 'Comic'),
+(19, 'NarutoL', 'Masashi Kishimoto', 5, '6488fd8b70b2e.jpg', 'Comic'),
+(21, 'Waifu', 'Ray', 4, '64890f2b6bda0.jpg', 'Romance');
 
 -- --------------------------------------------------------
 
@@ -58,9 +59,21 @@ INSERT INTO `buku` (`id`, `nama`, `pengarang`, `available`, `sampul`, `kategori`
 
 CREATE TABLE `peminjaman` (
   `id_pinjam` int(11) NOT NULL,
-  `tgl_pinjam` date NOT NULL,
-  `tgl_kembalian` date NOT NULL
+  `id_user` int(11) NOT NULL,
+  `id_pustakawan` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `tgl_pinjam` date DEFAULT NULL,
+  `tgl_kembalian` date DEFAULT NULL,
+  `status_peminjaman` enum('pending','disetujui','ditolak','dipinjam','dikembalikan') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`id_pinjam`, `id_user`, `id_pustakawan`, `id_buku`, `tgl_pinjam`, `tgl_kembalian`, `status_peminjaman`) VALUES
+(11, 3, 1, 8, '0000-00-00', '0000-00-00', 'pending'),
+(12, 3, 1, 9, '0000-00-00', '0000-00-00', 'dipinjam');
 
 -- --------------------------------------------------------
 
@@ -69,22 +82,21 @@ CREATE TABLE `peminjaman` (
 --
 
 CREATE TABLE `pustakawan` (
-  `username` varchar(20) NOT NULL,
+  `id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
   `password` varchar(50) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `nohp` varchar(15) NOT NULL,
-  `foto` blob DEFAULT NULL
+  `foto` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `pustakawan`
 --
 
-INSERT INTO `pustakawan` (`username`, `password`, `nama`, `email`, `nohp`, `foto`) VALUES
-('billie', 'joe', 'billie joe', 'bjarsmtrong@librarian.uns.ac.id', '0859999999', NULL),
-('joe', 'sam', 'joe', 'bjj@librarian.uns.ac.id', '081111112333', NULL),
-('tulus', 'admin', 'Tulus Toto Raharjo', 'TulusTR@librarian.uns.ac.id', '089655847697', NULL);
+INSERT INTO `pustakawan` (`id`, `username`, `password`, `nama`, `email`, `nohp`, `foto`) VALUES
+(1, 'admin', 'admin', 'admin', 'admin@email.co.id', '081234567890', NULL);
 
 -- --------------------------------------------------------
 
@@ -100,16 +112,17 @@ CREATE TABLE `user` (
   `email` varchar(100) DEFAULT NULL,
   `nohp` varchar(15) DEFAULT NULL,
   `nim` varchar(10) DEFAULT NULL,
-  `status` enum('aktif','suspend') NOT NULL
+  `status` enum('aktif','suspend','inactive') NOT NULL,
+  `foto` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `nama`, `email`, `nohp`, `nim`, `status`) VALUES
-(3, 'ray', 'chi', 'Rio', '', '', '', 'aktif'),
-(4, 'zzz', 'kkk', 'xyz', '', '', '', 'aktif');
+INSERT INTO `user` (`id`, `username`, `password`, `nama`, `email`, `nohp`, `nim`, `status`, `foto`) VALUES
+(3, 'ray', 'chi', 'Rio', 'rio.ae23@gmail.com', '', '', 'aktif', '64890530e550a.jpg'),
+(4, 'zzz', 'kkk', 'xyz', '', '', '', 'aktif', NULL);
 
 --
 -- Indexes for dumped tables
@@ -122,10 +135,20 @@ ALTER TABLE `buku`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_pinjam`),
+  ADD KEY `userFK` (`id_user`),
+  ADD KEY `bookFK` (`id_buku`),
+  ADD KEY `adminFK` (`id_pustakawan`);
+
+--
 -- Indexes for table `pustakawan`
 --
 ALTER TABLE `pustakawan`
-  ADD PRIMARY KEY (`username`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username_pustakawan` (`username`);
 
 --
 -- Indexes for table `user`
@@ -141,13 +164,37 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_pinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `pustakawan`
+--
+ALTER TABLE `pustakawan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `adminFK` FOREIGN KEY (`id_pustakawan`) REFERENCES `pustakawan` (`id`),
+  ADD CONSTRAINT `bookFK` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id`),
+  ADD CONSTRAINT `userFK` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
