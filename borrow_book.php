66bfec13d3a0ab4e76d13dce426a2b6625c9dc12
Tulus Toto Raharjo <totoraharjotulus@gmail.com>
2023-06-18 08:54:55 +0700

    update FE borrow_book

--- borrow_book.php
+++ borrow_book.php
@@ -35,38 +35,39 @@ if(!isset($_SESSION["login"])){
     <title>Librarow</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/user_page.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 
 <body style="background-color: #D0D0D0;">
 
     <!--Navbar-->
     <nav class="navbar sticky-top navbar-expand-sm navbar-dark" style="color: #D0EFFF;">
-        <div class="container-fluid ">
-            <a class="navbar-brand " href="#">LIBRAROW</a>
-            <div class="nav navbar-custom" id="navbarSupportedContent" style="color: #D0EFFF !important;">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="user_page.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="book_categories.php">Categories</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="user_profile.php">Profile</a>
-                    </li>
-                </ul>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="user_page.php"><img src="img/logo.png" alt="logo" width="30"> LIBRAROW</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                <a class="nav-link" href="user_page.php" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home" data-bs-animation="true">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="book_categories.php" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Categories" data-bs-animation="true">Categories</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="user_profile.php" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Profile" data-bs-animation="true">Profile</a>
+                </li>
+            </ul>
             </div>
         </div>
     </nav>
 
-    <div class="bbcontainer">
+    <div class="bbcontainer" style="padding-bottom:50px">
         <div class="bbframe">
             <div class="bbimgcon">
                 <img src="img/<?=$book['sampul']?>" alt="">
             </div>
         </div>
         <div class="bbtextcon">
-            <div class="bbtitle">
+            <div class="bbtitle" style="margin-bottom: 50px;">
                 <p><?= $book['nama'] ?></p>
             </div>
             <div class="bbdesc">
@@ -90,13 +91,38 @@ if(!isset($_SESSION["login"])){
     </div>
 
     <form action="" method="post">
-        <input type="hidden" name="id_user"id="id_user" value="<?=$id_user?>">
-        <input type="hidden" name="id_buku"id="id_buku" value="<?=$id?>">
-        <input type="hidden" name="id_pustakawan"id="id_pustakawan" value="1">
-        <button class="buttonbb" type="submit" name="submit" onclick="return confirm('Pinjam buku <?=$booktitle?>?');">Borrow</button>
+        <input type="hidden" name="id_user" id="id_user" value="<?=$id_user?>">
+        <input type="hidden" name="id_buku" id="id_buku" value="<?=$id?>">
+        <input type="hidden" name="id_pustakawan" id="id_pustakawan" value="1">
+        <button class="buttonbb" type="button" data-toggle="modal" data-target="#myModal">Borrow</button>
     </form>
 
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="myModalLabel">Confirm Borrow</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Pinjam buku <?=$booktitle?>?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary btn-animate" data-dismiss="modal">Cancel</button>
+        <button type="submit" class="btn btn-primary btn-animate" name="submit" onclick="return confirm('Pinjam buku <?=$booktitle?>?');">Borrow</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <br><br>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
