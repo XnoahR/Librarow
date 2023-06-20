12f550d2b2977fe8af7f1ce0db615559d368c560
Tulus Toto Raharjo <totoraharjotulus@gmail.com>
2023-06-20 10:26:07 +0700

    update FE pengembalian buku & riwayat peminjaman

--- user_pengembalian_buku.php
+++ user_pengembalian_buku.php
@@ -8,62 +8,177 @@
     <title>Librarow</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/user_page.css">
+    <style>
+        body {
+            background-color: #D0D0D0;
+        }
+
+        h1 {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 0 auto;
+        }
+
+        table {
+            width: 100%;
+            margin-bottom: 20px;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        .form-label-judul {
+            font-weight: bold;
+            padding: 10px;
+        }
+
+        .judul-list {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        .judul-list li {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            border-bottom: 1px solid #ccc;
+            padding: 10px;
+        }
+
+        .form-button-kembalikan {
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            padding: 5px 10px;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        .form-button-kembalikan:hover {
+            background-color: #0056b3;
+        }
+
+        .buttonbb {
+            display: block;
+            width: 100%;
+            max-width: 200px;
+            margin: 0 auto;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            padding: 10px 20px;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        .buttonbb:hover {
+            background-color: #0056b3;
+        }
+
+        .pagination {
+            display: flex;
+            justify-content: center;
+            margin-top: 20px;
+        }
+
+        .page-item.active .page-link {
+            background-color: #007bff;
+            border-color: #007bff;
+        }
+    </style>
 </head>
 
-<body style="background-color: #D0D0D0;">
-
+<body>
     <!-- Navbar -->
     <nav class="navbar sticky-top navbar-expand-sm navbar-dark" style="color: #D0EFFF;">
-        <div class="container-fluid ">
-            <a class="navbar-brand " href="#">LIBRAROW</a>
-            <div class="nav navbar-custom" id="navbarSupportedContent" style="color: #D0EFFF !important;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="user_page.php"><img src="img/logo.png" alt="logo" width="30"> LIBRAROW</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="user_page.php">Home</a>
+                        <a class="nav-link" href="user_page.php" data-bs-toggle="tooltip" data-bs-placement="bottom"
+                            title="Home" data-bs-animation="true">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="book_categories.php">Categories</a>
+                        <a class="nav-link" href="book_categories.php" data-bs-toggle="tooltip"
+                            data-bs-placement="bottom" title="Categories" data-bs-animation="true">Categories</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="user_profile.php">Profile</a>
+                        <a class="nav-link" href="user_profile.php" data-bs-toggle="tooltip"
+                            data-bs-placement="bottom" title="Profile" data-bs-animation="true">Profile</a>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
-    
+
     <!-- Pengembalian Buku -->
     <h1>Form Pengembalian Buku</h1>
     <form>
-    <table>
-        <tr>
-        <td><label for="judul" class="form-label-judul">Judul Buku:</label></td>
-        </tr>
-        <tr>
-        <td>
-            <ul class="judul-list">
-            <li>
-                <p>Judul Buku 1</p>
-                <button type="button" class="form-button-kembalikan">Kembalikan</button>
-            </li>
-            <li>
-                <p>Judul Buku 2</p>
-                <button type="button" class="form-button-kembalikan">Kembalikan</button>
-            </li>
-            <li>
-                <p>Judul Buku 3</p>
-                <button type="button" class="form-button-kembalikan">Kembalikan</button>
-            </li>
-            </ul>
-        </td>
-        </tr>
-    </table>
-    </form>
+        <table>
+            <tr>
+                <td><label for="judul" class="form-label-judul">Judul Buku:</label></td>
+            </tr>
+            <tr>
+                <td>
+                    <ul class="judul-list">
+                        <li>
+                            <p>Judul Buku 1</p>
+                            <button type="button" class="form-button-kembalikan">Kembalikan</button>
+                        </li>
+                        <li>
+                            <p>Judul Buku 2</p>
+                            <button type="button" class="form-button-kembalikan">Kembalikan</button>
+                        </li>
+                        <li>
+                            <p>Judul Buku 3</p>
+                            <button type="button" class="form-button-kembalikan">Kembalikan</button>
+                        </li>
+                        <li>
+                            <p>Judul Buku 4</p>
+                            <button type="button" class="form-button-kembalikan">Kembalikan</button>
+                        </li>
+                        <li>
+                            <p>Judul Buku 5</p>
+                            <button type="button" class="form-button-kembalikan">Kembalikan</button>
+                        </li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
 
+        <div class="pagination">
+            <ul class="pagination">
+                <li class="page-item disabled">
+                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+                </li>
+                <li class="page-item active" aria-current="page">
+                    <a class="page-link" href="#">1</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">2</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">3</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                </li>
+            </ul>
+        </div>
+
+    </form>
 
     <div>
         <button class="buttonbb">Save Changes</button>
     </div>
-    </form>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
--- user_riwayat_peminjaman.php
+++ user_riwayat_peminjaman.php
@@ -22,60 +22,80 @@ if(!isset($_SESSION["login"])){
 
     <!--Navbar-->
     <nav class="navbar sticky-top navbar-expand-sm navbar-dark" style="color: #D0EFFF;">
-        <div class="container-fluid ">
-            <a class="navbar-brand " href="#">LIBRAROW</a>
-            <div class="nav navbar-custom" id="navbarSupportedContent" style="color: #D0EFFF !important;">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="user_page.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="book_categories.php">Categories</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="user_profile.php">Profile</a>
-                    </li>
-                </ul>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="user_page.php"><img src="img/logo.png" alt="logo" width="30"> LIBRAROW</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                <a class="nav-link" href="user_page.php" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home" data-bs-animation="true">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="book_categories.php" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Categories" data-bs-animation="true">Categories</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="user_profile.php" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Profile" data-bs-animation="true">Profile</a>
+                </li>
+            </ul>
             </div>
         </div>
     </nav>
+<!-- Riwayat Peminjaman Section -->
+<section class="riwayat-peminjaman-section">
+    <div class="container">
+        <h2>Riwayat Peminjaman Buku</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>Judul Buku</th>
+                    <th>Tanggal Peminjaman</th>
+                    <th>Tanggal Pengembalian</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- Data peminjaman -->
+                <tr>
+                    <td>1</td>
+                    <td>Harry Potter and the Philosopher's Stone</td>
+                    <td>2023-05-15</td>
+                    <td>2023-05-30</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>To Kill a Mockingbird</td>
+                    <td>2023-04-20</td>
+                    <td>2023-05-05</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>The Great Gatsby</td>
+                    <td>2023-03-10</td>
+                    <td>2023-03-25</td>
+                </tr>
+                <!-- End of data peminjaman -->
+            </tbody>
+        </table>
 
-    <!-- Riwayat Peminjaman Section -->
-    <section class="riwayat-peminjaman-section">
-        <div class="container">
-            <h2>Riwayat Peminjaman Buku</h2>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>Judul Buku</th>
-                        <th>Tanggal Peminjaman</th>
-                        <th>Tanggal Pengembalian</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Harry Potter and the Philosopher's Stone</td>
-                        <td>2023-05-15</td>
-                        <td>2023-05-30</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>To Kill a Mockingbird</td>
-                        <td>2023-04-20</td>
-                        <td>2023-05-05</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>The Great Gatsby</td>
-                        <td>2023-03-10</td>
-                        <td>2023-03-25</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </section>
+        <!-- Pagination -->
+        <nav aria-label="Page navigation">
+            <ul class="pagination">
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Previous">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+                <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</section>
 
     <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
