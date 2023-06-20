64fcac6ced188e9dd2919ce3b4b335389d7720e1
Tulus Toto Raharjo <totoraharjotulus@gmail.com>
2023-06-20 19:51:20 +0700

    updaet data admin FE

--- data_admin.php
+++ data_admin.php
@@ -20,11 +20,11 @@ $pustakawan = query("SELECT * FROM pustakawan");
 </head>
 
 <body style="background-color: #D0D0D0;">
-    <!--Navbar-->
-    <nav class="navbar sticky-top navbar-expand-sm navbar-dark" style="color: #D0EFFF;">
-        <div class="container-fluid ">
-            <a class="navbar-brand " href="login.php">LIBRAROW</a>
-            <div class="nav navbar-custom" id="navbarSupportedContent" style="color: #D0EFFF !important;">
+    <!-- Navbar -->
+    <nav class="navbar sticky-top navbar-expand-sm navbar-dark" style="background-color: #D0EFFF;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="login.php">LIBRAROW</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="admin_page.php">Home</a>
@@ -37,38 +37,52 @@ $pustakawan = query("SELECT * FROM pustakawan");
         </div>
     </nav>
 
-    <div class="datacontainer">
-        <div class="datatitle">Data Admin</div>
-        <div class="datasearch">Search
-            <input type="text" name="search" id="search">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        Data Admin
+                    </div>
+                    <div class="card-body">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text">Search</span>
+                            <input type="text" class="form-control" name="search" id="search">
+                        </div>
+
+                        <table class="table table-bordered table-striped">
+                            <thead class="table-primary">
+                                <tr>
+                                    <th scope="col">No</th>
+                                    <th scope="col">Foto</th>
+                                    <th scope="col">Nama</th>
+                                    <th scope="col">Username</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Password</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $i = 1; ?>
+                                <?php foreach ($pustakawan as $lib) : ?>
+                                    <tr>
+                                        <th scope="row"><?= $i; ?></th>
+                                        <td><?= $lib["foto"] ?></td>
+                                        <td><?= $lib["nama"] ?></td>
+                                        <td><?= $lib["username"] ?></td>
+                                        <td><?= $lib["email"] ?></td>
+                                        <td><?= $lib["password"] ?></td>
+                                    </tr>
+                                    <?php $i++; ?>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <table border="1px solid" width="85%" style="border-collapse:collapse;margin:auto;">
-            <th>No</th>
-            <th>Foto</th>
-            <th>Nama</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Password</th>
-
-
-            <?php
-            $i = 1;
-            ?>
-            <?php foreach($pustakawan as $lib) : ?>
-
-            <tr>
-                <td><?=$i;?></td>
-                <td><?=$lib["foto"]?></td>
-                <td><?=$lib["nama"]?></td>
-                <td><?=$lib["username"]?></td>
-                <td><?=$lib["email"]?></td>
-                <td><?=$lib["password"]?></td>
-            </tr>
-            <?php $i++;?>
-            <?php endforeach;?>
-        </table>
     </div>
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
