26e4c317a27f57e5a976e4dd763ee8d48ee6303d
Tulus Toto Raharjo <totoraharjotulus@gmail.com>
2023-06-21 11:12:16 +0700

    Update informasi_mhs.php

--- informasi_mhs.php
+++ informasi_mhs.php
@@ -1,10 +1,10 @@
 <?php
 include 'functions.php';
 session_start();
-if(!isset($_SESSION["admin"])){
+if (!isset($_SESSION["admin"])) {
     header("Location:login.php");
     exit;
-} 
+}
 $id = $_GET['id'];
 $mahasiswa = query("SELECT * FROM user WHERE id = '$id'")[0];
 $peminjaman = query("SELECT id_buku FROM peminjaman WHERE id_user = '$id' AND status_peminjaman = 'dipinjam'");
@@ -36,26 +36,36 @@ foreach ($peminjaman as $kodebuku) {
 <body style="background-color: #D0D0D0;">
     <!--Navbar-->
     <nav class="navbar sticky-top navbar-expand-sm navbar-dark" style="color: #D0EFFF;">
-        <div class="container-fluid ">
-            <a class="navbar-brand " href="login.php">LIBRAROW</a>
-            <div class="nav navbar-custom" id="navbarSupportedContent" style="color: #D0EFFF !important;">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="admin_page.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="profile_pustakawan.php">Profile</a>
-                    </li>
-                </ul>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="user_page.php">
+                <img src="img/logo.png" alt="logo" width="30">LIBRAROW</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                <a class="nav-link" href="admin_page.php" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Home" data-bs-animation="true">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="profile_pustakawan.php" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Profile" data-bs-animation="true">Profile</a>
+                </li>
+            </ul>
             </div>
         </div>
     </nav>
-
     <div class="sicontainer">
+
+        <div class="siprofile">
+            <div class="simid">
+                <?php if ($mahasiswa['foto'] === NULL) : ?>
+                    <img class="rounded-circle shadow-lg border" src="img/orang.png" alt="">
+                <?php else : ?>
+                    <img class="rounded-circle shadow-lg border float-start me-3" src="img/<?= $mahasiswa['foto']; ?>" alt="">
+                <?php endif; ?>
+            </div>
+        </div>
         <div class="siinfo">
             <div class="siinfotext">
                 <div class="table-responsive">
-                    <table class="table">
+                    <table class="table table-bordered">
                         <tr>
                             <th width="40%">Nim</th>
                             <td><?= $mahasiswa['nim'] ?></td>
@@ -82,26 +92,19 @@ foreach ($peminjaman as $kodebuku) {
                         </tr>
                         <tr>
                             <th width="40%">Daftar buku dipinjam</th>
-                            <td><ol><?php foreach($info_buku as $daftar_buku) : ?>
-                                <li>  <?= $daftar_buku?></li>
-                                <?php endforeach;?></ol> 
+                            <td>
+                                <ol>
+                                    <?php foreach ($info_buku as $daftar_buku) : ?>
+                                        <li><?= $daftar_buku ?></li>
+                                    <?php endforeach; ?>
+                                </ol>
                             </td>
                         </tr>
                     </table>
                 </div>
-                <a href="data_mahasiswa.php"><button class="float-end btn btn-primary">Kembali</button></a>
+                <a href="data_mahasiswa.php" class="float-end btn btn-primary">Kembali</a>
             </div>
-        </div>
-        <div class="siprofile">
-            <div class="simid">
-                <?php if($mahasiswa['foto'] === NULL) :?>
-                <img class="rounded-circle shadow-lg border" src="img/orang.png" alt="">
-                <?php else :?>
-                    <img class="rounded-circle shadow-lg border" src="img/<?=$mahasiswa['foto'];?>" alt="">
-                
-                  <?php endif;?>
-                    
-                  </div>
+
 
         </div>
     </div>
