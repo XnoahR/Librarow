3c58690c95ee2d38223b204428e00351dc7b9561
Tulus Toto Raharjo <totoraharjotulus@gmail.com>
2023-06-21 09:47:08 +0700

    FE add book & data buku

--- add_book.php
+++ add_book.php
@@ -23,15 +23,47 @@ if (isset($_POST["submit"])) {
     <title>Librarow</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/admin_page.css">
+    <style>
+        body {
+            background-color: #f4f7fa;
+        }
+
+        .card {
+            border: none;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.4);
+            margin-bottom: 2em;
+        }
+
+        .card-header {
+            background-color: #1167b1;
+            color: #fff;
+            font-weight: bold;
+        }
+
+        .btn-primary {
+            background-color: #007bff;
+            border-color: #007bff;
+        }
+
+        .btn-primary:hover {
+            background-color: #0069d9;
+            border-color: #0062cc;
+        }
+    </style>
 </head>
 
-<body style="background-color: #D0D0D0;">
+<body>
 
-    <!--Navbar-->
-    <nav class="navbar sticky-top navbar-expand-sm navbar-dark" style="color: #D0EFFF;">
-        <div class="container-fluid ">
-            <a class="navbar-brand " href="login.php">LIBRAROW</a>
-            <div class="nav navbar-custom" id="navbarSupportedContent" style="color: #D0EFFF !important;">
+    <!-- Navbar -->
+    <nav class="navbar sticky-top navbar-expand-sm navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="login.php">LIBRAROW</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="admin_page.php">Home</a>
@@ -45,53 +77,70 @@ if (isset($_POST["submit"])) {
     </nav>
 
 
-    <!-- Tab Container -->
-    <div class="addbookbox">
-        <div class="insidebookbox">
-            <form action="" method="post" enctype="multipart/form-data">
-                <ul>
-                    <li>
-                        <label for="name">Judul Buku: </label>
-                        <input type="text" name="name" id="name">
-                    </li>
-                    <li>
-                        <label for="pengarang">Pengarang: </label>
-                        <input type="text" name="pengarang" id="pengarang">
-                    </li>
-                    <li>
-                        <label for="jumlah">Jumlah Buku: </label>
-                        <input type="number" name="jumlah" id="jumlah">
-                    </li>
-                    <li>
-                        <label for="sampul">Sampul Buku: </label>
-                        <input type="file" name="sampul" id="sampul">
-                    </li>
-
-                    <label for="kategori">Kategori Buku:</label>
-                    <li>
-                        <input type="radio" name="kategori" id="Horror" value="Horror">
-                        <label for="kategori" class="ilbl">Horor</label>
-                        <input type="radio" name="kategori" id="Drama" value="Drama">
-                        <label for="kategori" class="ilbl">Drama</label>
-                        <input type="radio" name="kategori" id="Biograph" value="Biograph">
-                        <label for="kategori" class="ilbl">Biograph</label>
-                        <input type="radio" name="kategori" id="Sociology" value="Sociology">
-                        <label for="kategori" class="ilbl">Sociology</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="kategori" id="Romance" value="Romance">
-                        <label for="kategori" class="ilbl">Romance</label>
-                        <input type="radio" name="kategori" id="Comic" value="Comic">
-                        <label for="kategori" class="ilbl">Comic</label>
-                    </li>
-                    <li>
-                        <button type="submit" name="submit">Add Book</button>
-                    </li>
-                </ul>
-            </form>
+    <!-- Form Add Book -->
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        Add Book
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="post" enctype="multipart/form-data">
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Judul Buku:</label>
+                                <input type="text" name="name" id="name" class="form-control" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="pengarang" class="form-label">Pengarang:</label>
+                                <input type="text" name="pengarang" id="pengarang" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="jumlah" class="form-label">Jumlah Buku:</label>
+                                <input type="number" name="jumlah" id="jumlah" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="sampul" class="form-label">Sampul Buku:</label>
+                                <input type="file" name="sampul" id="sampul" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="kategori" class="form-label">Kategori Buku:</label>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="kategori" id="horror" value="Horror">
+                                    <label class="form-check-label" for="horror">Horor</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="kategori" id="drama" value="Drama">
+                                    <label class="form-check-label" for="drama">Drama</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="kategori" id="biograph" value="Biograph">
+                                    <label class="form-check-label" for="biograph">Biograph</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="kategori" id="sociology" value="Sociology">
+                                    <label class="form-check-label" for="sociology">Sociology</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="kategori" id="romance" value="Romance">
+                                    <label class="form-check-label" for="romance">Romance</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="kategori" id="comic" value="Comic">
+                                    <label class="form-check-label" for="comic">Comic</label>
+                                </div>
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" name="submit" class="btn btn-primary">Add Book</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- data_buku.php
+++ data_buku.php
@@ -1,10 +1,10 @@
 <?php
 include 'functions.php';
 session_start();
-if(!isset($_SESSION["admin"])){
+if (!isset($_SESSION["admin"])) {
     header("Location:login.php");
     exit;
-} 
+}
 $buku = query("SELECT * FROM buku");
 
 $dataLimitPerPage = 5;
@@ -19,6 +19,7 @@ if (isset($_POST['cari'])) {
     $buku = query("SELECT * FROM buku LIMIT $dataStart, $dataLimitPerPage");
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,11 +33,11 @@ if (isset($_POST['cari'])) {
 </head>
 
 <body style="background-color: #D0D0D0;">
-    <!--Navbar-->
-    <nav class="navbar sticky-top navbar-expand-sm navbar-dark" style="color: #D0EFFF;">
-        <div class="container-fluid ">
-            <a class="navbar-brand " href="login.php">LIBRAROW</a>
-            <div class="nav navbar-custom" id="navbarSupportedContent" style="color: #D0EFFF !important;">
+    <!-- Navbar -->
+    <nav class="navbar sticky-top navbar-expand-sm navbar-dark" style="background-color: #D0EFFF;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="login.php">LIBRAROW</a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="admin_page.php">Home</a>
@@ -49,60 +50,76 @@ if (isset($_POST['cari'])) {
         </div>
     </nav>
 
-    <div class="datacontainer">
-        <div class="datatitle">Data Buku</div>
-        <div class="datasearch">
-        <form action="" method="post">
-                <input type="text" name="keyword" id="keyword" size="25" autocomplete="off" placeholder="Cari Buku">
-                <button class="btn-sm btn-outline-success" type="submit" name="cari">Cari</button>
-            </form>
+    <div class="container">
+
+        <div class="datacontainer">
+            <div class="datatitle">Data Buku</div>
+            <div class="datasearch">
+                <form action="" method="post" class="form-inline">
+                    <div class="input-group">
+                        <input type="text" name="keyword" id="keyword" size="25" autocomplete="off" placeholder="Cari Buku" class="form-control">
+                        <div class="input-group-append">
+                            <button class="btn btn-success" type="submit" name="cari">Cari</button>
+
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <a href="add_book.php" class="btn btn-primary">Tambah Buku</a>
+            <table class="table table-bordered table-sm mt-3">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Sampul</th>
+                        <th>Judul</th>
+                        <th>Ketersediaan</th>
+                        <th>Pengarang</th>
+                        <th>Kategori</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($buku as $book) : ?>
+                        <tr>
+                            <td class="p-1"><?= $i; ?></td>
+                            <td class="p-1"><img src="img/<?= $book["sampul"] ?>" alt="<?= $book["nama"] ?>" width="50"></td>
+                            <td class="p-1"><?= $book["nama"] ?></td>
+                            <td class="p-1"><?= $book["available"] ?></td>
+                            <td class="p-1"><?= $book["pengarang"] ?></td>
+                            <td class="p-1"><?= $book["kategori"] ?></td>
+                            <td class="p-1">
+                                <a href="edit_book.php?id=<?= $book['id'] ?>" class="btn btn-sm btn-primary">edit</a>
+                                <a href="delete_book.php?id=<?= $book['id'] ?>" onclick="return confirm('Delete Item?');" class="btn btn-sm btn-danger">delete</a>
+                            </td>
+                        </tr>
+                        <?php $i++; ?>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+
+
+
+
         </div>
-        <a href="add_book.php">Tambah Buku</a>
-        <table border="1px solid" width="95%" style="border-collapse:collapse;margin:auto;">
-            <th>No</th>
-            <th>Sampul</th>
-            <th>Judul</th>
-            <th>Ketersediaan</th>
-            <th>Pengarang</th>
-            <th>Kategori</th>
-            <th>Aksi</th>
-
-            <?php
-            $i = 1;
-            ?>
-            <?php foreach($buku as $book) : ?>
-
-            <tr>
-                <td><?=$i;?></td>
-                <td><img src="img/<?=$book["sampul"]?>" alt="<?=$book["nama"]?>"width="50"></td>
-                <td><?=$book["nama"]?></td>
-                <td><?=$book["available"]?></td>
-                
-                <td><?=$book["pengarang"]?></td>
-                <td><?=$book["kategori"]?></td>
-                <td>
-                    <a href="edit_book.php?id=<?=$book['id']?>">edit</a> |
-                    <a href="delete_book.php?id=<?=$book['id']?>"onclick="return confirm('Delete Item?');">delete</a>
-                </td>
-            </tr>
-            <?php $i++;?>
-            <?php endforeach;?>
-        </table>
-        <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-center">
-                <?php if ($activePage > 1) : ?>
-                    <li class="page-item"><a class="page-link" href="?page=<?= $activePage -= 1; ?>"><</a>
-                    </li>
-                <?php endif; ?>
-                <?php for ($i = 1; $i <= $pageTotal; $i++) : ?>
-                    <li class="page-item"><a class="page-link" href="?page=<?= $i; ?>"><?= $i ?></a></li>
-                <?php endfor; ?>
-                <?php if ($activePage < $pageTotal) : ?>
-                    <li class="page-item"><a class="page-link" href="?page=<?= $activePage += 1; ?>">></a></li>
-                <?php endif; ?>
-            </ul>
-        </nav>
     </div>
+    <br><br><br>
+    <nav aria-label="Page navigation example" style="padding-bottom: 20px;">
+        <ul class="pagination justify-content-center">
+            <?php if ($activePage > 1) : ?>
+                <li class="page-item"><a class="page-link" href="?page=<?= $activePage - 1; ?>">
+                        << /a>
+                </li>
+            <?php endif; ?>
+            <?php for ($i = 1; $i <= $pageTotal; $i++) : ?>
+                <li class="page-item <?= ($i == $activePage) ? 'active' : ''; ?>"><a class="page-link" href="?page=<?= $i; ?>"><?= $i ?></a></li>
+            <?php endfor; ?>
+            <?php if ($activePage < $pageTotal) : ?>
+                <li class="page-item"><a class="page-link" href="?page=<?= $activePage + 1; ?>">></a></li>
+            <?php endif; ?>
+        </ul>
+    </nav>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 
 </html>
